7b02522bc1eff7a1099949612a0bed9824ca45d8
Hyewon3155 <lucybeauty1211@gmail.com>
2023-04-18 15:24:55 +0900

    nickname, name, email duplicate check

--- src/main/java/com/koreaIT/demo/util/Util.java
+++ src/main/java/com/koreaIT/demo/util/Util.java
@@ -11,9 +11,18 @@ public class Util {
 //		if (obj instanceof String == false) {
 //			return true;
 //		}
+		// 타입이 같은지 검사하는 것
+		// obj가 String 타입 --> return true
+		// obj가 String 타입 X --> return str.trim().length()
 		
 		String str = (String) obj;
 		
 		return str.trim().length() == 0;
+	}
+	
+	public static Object f(String format, Object...args) {
+		return String.format(format, args);
+		//Object...args --> Object형인 여러 개의 인자를 받을 수 있음
+		//매개변수의 갯수 제한이 사라짐!!
 	}
 }
